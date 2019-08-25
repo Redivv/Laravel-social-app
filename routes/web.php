bcbbc0e13a97697b72b8b8194f80182fb54d4b8c
Redivv <j.rajca45@gmail.com>
2019-08-25 20:01:35 +0200

    Changed routes, searcher route added

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,9 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/searcher', 'SearchController@index');
+
+Route::prefix('user')->group(function(){
+    Route::get('/home', 'HomeController@index');
+});
+
