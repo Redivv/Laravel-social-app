e264561086720f508a2976ed48f76a960d63646f
PSb19 <33777820+PSb19@users.noreply.github.com>
2019-09-10 22:52:47 +0200

    Minor Improvements 10.09.19' 23:00

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,9 +12,10 @@
                         <form method="POST" action="{{ route('login') }}">
                             @csrf
 
-                            <div class="form-group row">
+                            <div class=" row justify-content-center">
                                 <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
+                            </div>
+                            <div class="form-group row justify-content-center">
                                 <div class="col-md-6">
                                     <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
@@ -26,9 +27,10 @@
                                 </div>
                             </div>
 
-                            <div class="form-group row">
+                            <div class=" row justify-content-center">
                                 <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
+                            </div>
+                            <div class="form-group row justify-content-center">
                                 <div class="col-md-6">
                                     <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
