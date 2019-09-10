e6ae14f32435637dbb0f6897b0fd686076a9ae59
PSb19 <33777820+PSb19@users.noreply.github.com>
2019-09-10 22:35:19 +0200

    A bit of seasoning on our humble page.

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -7,7 +7,7 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
+    <!-- {{ config('app.name', 'Laravel') }} -->
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
