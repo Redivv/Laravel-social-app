b63047816110d3d434c3ce69870488d514670589
PSb19 <33777820+PSb19@users.noreply.github.com>
2019-09-18 17:00:56 +0200

    Added city & description

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -11,19 +11,37 @@
             </div>
             <div class="row form-group">
                 <div class="col-md-6">
-                    <input id="name" type="text" size="15" class="form-control" name="name" value="">
+                    <input id="name" type="text" size="15" class="form-control" name="name" placeholder="{{ __('name') }}">
                 </div>
             </div>
+
             <div class="row">
                     <label for="surname" class="col-md-4 col-form-label">{{ __('surname')  }}</label>
-                </div>
-                <div class="row form-group">
-                    <div class="col-md-6">
-                        <input id="surname" type="text" size="15" class="form-control" name="surname" value="">
-                    </div>
+            </div>
+            <div class="row form-group">
+                <div class="col-md-6">
+                    <input id="surname" type="text" size="15" class="form-control" name="surname" placeholder="{{ __('surname') }}">
                 </div>
             </div>
-            <button type="submit" class="">Submit</button>
+
+            <div class="row">
+                <label for="city" class="col-md-4 col-form-label">{{ __('city')  }}</label>
+        </div>
+        <div class="row form-group">
+            <div class="col-md-6">
+                <input id="city" type="text" size="15" class="form-control" name="city" placeholder="{{ __('city') }}">
+            </div>
+        </div>
+
+            <div class="row">
+                <label for="description" class="col-md-4 col-form-label">{{ __('description')  }}</label>
+            </div>
+            <div class="row form-group">
+                <div class="col-md-6">
+                    <textarea id="description" type="text" cols="5" rows="4" class="form-control" name="description" placeholder="Opis"></textarea>
+                </div>
+            </div>
+            <button type="submit">Submit</button>
         </form>
     </div>
 @endsection
