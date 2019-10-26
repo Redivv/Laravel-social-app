f799b95c6e6ea0a167c048b46680ad6219fa3f84
PSb19 <33777820+PSb19@users.noreply.github.com>
2019-10-26 12:08:55 +0200

    Still 30 errors

--- database/factories/CityFactory.php
+++ database/factories/CityFactory.php
@@ -1,10 +1,20 @@
 <?php
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
-
 use App\City;
 use Illuminate\Support\Str;
 use Faker\Generator as Faker;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
 
 $factory->define(City::class, function (Faker $faker) {
     $city_slug =  $faker->unique()->city;
