38c02e008092f9e726b6f08672aab6b7e62420c1
Redivv <j.rajca45@gmail.com>
2020-01-14 00:55:53 +0100

    fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('queue:work --tries=3')
-            ->cron('* * * * * *')
+            ->cron('* * * * *')
             ->withoutOverlapping();
     }
 
